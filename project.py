cb860188c6923145d0412c78109002047115d2a2
prateek <agarwalprateek666@gmail.com>
2022-10-11 19:32:18 +0530

    python rps edit

--- project.py
+++ project.py
@@ -1,24 +1,32 @@
+from optparse import Option
 from random import choice
-pick=['R','P','S']
-user_input=input("choose your move([R]rock/[P]paper/[S]scissior)")
-computer_input=choice(pick)
-if computer_input == 'R' and user_input == 'R':
-    print("It's a draw")
-elif computer_input == 'P' and user_input == 'P':
-     print("It's a draw")
-elif computer_input == 'S' and user_input == 'S':
-    print("It's a draw")
-elif computer_input == 'R' and user_input == 'P':
-    print("You won")
-elif computer_input == 'P' and user_input == 'S':
-   print("You won")
-elif computer_input == 'S' and user_input == 'R':
-    print("You won")
-elif computer_input == 'R' and user_input == 'S':
-    print("You loose")
-elif  computer_input == 'S' and user_input == 'P':
-    print("You loose") 
-elif   computer_input == 'P' and user_input == 'R':
-    print("You loose")
-else:
-    print("Give a valid input") 
+while True:
+    #computer input
+    pick=['R','P','S']
+    computer_input=choice(pick)
+    #user input
+    user_input=input("choose your move([R]rock/[P]paper/[S]scissior)")
+    #all possibilities
+    if computer_input == 'R' and user_input == 'R':
+        print("It's a draw")
+    elif computer_input == 'P' and user_input == 'P':
+        print("It's a draw")
+    elif computer_input == 'S' and user_input == 'S':
+        print("It's a draw")
+    elif computer_input == 'R' and user_input == 'P':
+        print("You won")
+    elif computer_input == 'P' and user_input == 'S':
+        print("You won")
+    elif computer_input == 'S' and user_input == 'R':
+        print("You won")
+    elif computer_input == 'R' and user_input == 'S':
+        print("You loose")
+    elif  computer_input == 'S' and user_input == 'P':
+        print("You loose") 
+    elif   computer_input == 'P' and user_input == 'R':
+        print("You loose")
+    else:
+        print("Give a valid input")
+    Option=int(input("Enter 0 if you want to play again:"))
+    if (Option):
+        break 
